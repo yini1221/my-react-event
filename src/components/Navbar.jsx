a5f15dee32e5f40a33db4c44bce9203f77fa7e81
Yini <oopsohya240@gmail.com>
2025-05-19 23:40:07 +0800

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,26 +4,31 @@ import ToggleTheme from './ToggleTheme';
 function Navbar()
 {
   return (
-    <nav className="navbar navbar-expand navbar-dark bg-secondary" style={{ '--bs-bg-opacity': '.5' }}>
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/home">Logo</Link>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav">
-            <li className="nav-item"><Link className="nav-link custom-link" to="home">首頁</Link></li>
-            <li className="nav-item"><Link className="nav-link custom-link" to="/events">活動</Link></li>
-            <li className="nav-item"><Link className="nav-link custom-link" to="/favorites">收藏</Link></li>
-            <li className="nav-item"><Link className="nav-link custom-link" to="/profile">個人資訊</Link></li>
-            <li className="nav-item"><Link className="nav-link custom-link" to="/admin">後台管理</Link></li>
-            <li className="nav-item"><ToggleTheme /></li>
-          </ul>
-          <form className="d-flex g-3 align-items-center">
-            <input className="col-auto form-control" type="search" placeholder="搜尋..." aria-label="Search" />
-            <button className="col-auto btn btn-outline-secondary" type="submit">搜尋</button>
-          </form>
+    <>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse bg-light" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item"><Link className="nav-link custom-link" to="home">首頁</Link></li>
+                  <li className="nav-item"><Link className="nav-link custom-link" to="/events">活動</Link></li>
+                  <li className="nav-item"><Link className="nav-link custom-link" to="/favorites">收藏</Link></li>
+                  <li className="nav-item"><Link className="nav-link custom-link" to="/profile">個人資訊</Link></li>
+                  <li className="nav-item"><Link className="nav-link custom-link" to="/admin">後台管理</Link></li>
+                  <li className="nav-item"><ToggleTheme /></li>
+            </ul>
+            <form class="d-flex me-2">
+              <input class="form-control me-2" type="search" placeholder="搜尋..." aria-label="Search" />
+              <button class="btn btn-outline-secondary" type="submit">Search</button>
+            </form>
+            <Link className="custom-link" to="/admin">登入</Link>
+          </div>
         </div>
-        <Link className="custom-link" to="/admin">登入</Link>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
 
